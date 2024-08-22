e8908c414707f0b324f7c40580ab6161208f3a0d
Gregory <glrd4726ml@gmail.com>
2024-08-22 14:05:26 -0300

    Probando

--- Block/block.js
+++ Block/block.js
@@ -1,3 +1,4 @@
+const SHA256 = require('crypto-js/sha256')
 
 class Block{
     constructor(timestamp, lasthash, hash,data){
@@ -13,6 +14,17 @@ class Block{
                 Last Hash: ${this.lasthash}
                 Hash: ${this.hash}
                 Data: ${this.data}`;
+    }
+
+    static genesis(){
+        return new this(Date.UTC(), '0'.repeat(64), '0'.repeat(64), [])
+    }
+
+    static mineBlock(lastBlock, data){
+        const timestamp = Date.UTC();
+        const lasthash = lastBlock.hash;
+        const hash = '0'.repeat(64);
+        return new this(timestamp, lasthash, hash, data)
     }
 }
 
